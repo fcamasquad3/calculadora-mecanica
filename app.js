3065170e1c60bc9a7349f0418fbac724bd984e90
Andrew Kalil Barrense Magalhães <kalilmagal@gmail.com>
2021-04-27 19:27:29 -0300

    add function deletar

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let valorTela = document.getElementById("tela")
+
 //adiciona na tela da calculadora o caractere (número/operação) selecionado
 function adicionarNaTela (tela,caractere) {
   if (tela.value == null || tela.value == 0) {
@@ -5,4 +7,15 @@ function adicionarNaTela (tela,caractere) {
   } else {
     tela.value += caractere
   }
+}
+
+//pega a string formada na tela e computa o resultado
+function mostrarResultado() {
+  valorTela.value = eval(valorTela.value)
+}
+
+//deletar o último caractere inserido
+function deletar() {
+  let ultimo = valorTela.value.length - 1
+  valorTela.value = valorTela.value.slice(0, ultimo)
 }
