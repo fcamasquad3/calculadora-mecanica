d33018d99ce50c3bf20f7d48b03706a97833c148
Drielison Lopes <drielisonl@gmail.com>
2021-04-29 11:27:46 -0300

    CSS do index
    Ajuste no CSS do index e intro de JS da freelance

--- app.js
+++ app.js
@@ -1,50 +1,25 @@
-let valorTela = document.getElementById("tela")
+//IDEIA INICIAL
 
-//adiciona na tela da calculadora o caractere (número/operação) selecionado
-function adicionarNaTela (tela,caractere) {
-  if (tela.value == null || tela.value == 0) {
-    tela.value = caractere
-  } else {
-    tela.value += caractere
-  }
-}
+let nomeJob = document.getElementById("nomeJob")
 
-//pega a string formada na tela e computa o resultado
-function mostrarResultado() {
-  valorTela.value = eval(valorTela.value)
-}
+let valorMes = document.getElementById("valorMes")
+let valorHorasDia = document.getElementById("valorHorasDia")
+let valorSemana = document.getElementById("valorSemana")
+let horasJob = document.getElementById("horasJob")
+let valorHoraTrabalho = document.getElementById("valorHoraTrabalho")
 
-//deletar o último caractere inserido
-function deletar() {
-  let ultimo = valorTela.value.length - 1
-  valorTela.value = valorTela.value.slice(0, ultimo)
-}
 
-//
-function porcento() {
-  let total = ""
-  let contaPorcento = ""
-  let valorPorcento = ""
-  
-  if (valorTela.value.includes("%")) {
-    for (let i = valorTela.value.length - 2; i >= 0; i--) {
-      if (isNaN(parseInt(valorTela.value[i])) === false) {
-        contaPorcento += valorTela.value[i]
-      } else {
-        break
-      }
-    }
+// ex: 4000 = 6 / 4 / 4
+valorHoraTrabalho = valorMes.value / valorHorasDia.value / valorSemana.value
 
-    for (let i = 0; i >= 0; i++) {
-      if (isNaN(parseInt(valorTela.value[i])) === false) {
-        total += valorTela.value[i]
-      } else {
-        break
-      }
-    }
-    contaPorcento = contaPorcento.split("").reverse().join("")
-    valorPorcento = parseInt(total) * (parseInt(contaPorcento) / 100)
-  }
 
-  return valorPorcento
-}
+function calculate () {
+  let resultado = 0;
+
+  resultado = valorHoraTrabalho * horasJob;
+
+  return resultado                                   
+} 
+
+//Novas alterações
+// valor do Job ${nome.Job}:
