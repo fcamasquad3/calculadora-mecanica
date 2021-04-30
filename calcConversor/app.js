32334bf389967d1b3a55149dcc7687c1bb3dbc5f
barrosiam <samanta464.barros@gmail.com>
2021-04-30 03:10:05 -0300

    Atualização nos selects

--- calcConversor/app.js
+++ calcConversor/app.js
@@ -1,4 +1,7 @@
-let valorTela = document.getElementById("tela")
+const valorTela = document.getElementById("tela");
+const cbMedidas = document.getElementById("op-conversores")
+let option = cbMedidas.options[cbMedidas.selectedIndex]
+
 
 //adiciona na tela da calculadora o caractere (número/operação) selecionado
 function adicionarNaTela (tela,caractere) {
@@ -18,4 +21,69 @@ function mostrarResultado() {
 function deletar() {
   let ultimo = valorTela.value.length - 1
   valorTela.value = valorTela.value.slice(0, ultimo)
+}
+
+
+// function Atualiza(){
+//   const cbMedidas = document.getElementById("op-conversores")
+//   let option = cbMedidas.options[cbMedidas.selectedIndex];
+
+//   switch(option)
+//   {
+//     case 1:
+//       document
+//       break;
+//   }
+// }
+
+
+function Teste(){
+  // let option = cbMedidas.value.options[cbMedidas.selectedIndex];
+  const tipoMedidas = document.getElementById("op-tipo1");
+  let option = tipoMedidas.options[0];
+
+  const tipoMedidas2 = document.getElementById("op-tipo2");
+  let option = tipoMedidas2.options[0];
+  
+  if (cbMedidas.value=="Medidas") {   
+
+    option.innerText = "Quilômetro"
+    let option2 = option.nextElementSibling
+    option2.innerText = "Milhas"
+    let option3 = option2.nextElementSibling
+    option3.innerText = "Metro"
+    let option4 = option3.nextElementSibling
+    option4.innerText = "Centímetros"
+    let option5 = option4.nextElementSibling
+    option5.innerText = "Pés"
+    let option6 = option5.nextElementSibling
+    option6.innerText = "Polegadas"   
+  }
+  
+  else if (cbMedidas.value=="Temperatura"){   
+
+    option.innerText = "Celsius"
+    let option2 = option.nextElementSibling
+    option2.innerText = "Fahrenheit"
+    let option3 = option2.nextElementSibling
+    option3.innerText = ""
+    let option4 = option3.nextElementSibling
+    option4.innerText = ""
+    let option5 = option4.nextElementSibling
+    option5.innerText = ""
+    let option6 = option5.nextElementSibling
+    option6.innerText = "" 
+   
+  }
+  else {  
+    option.innerText = "Byte"
+    let option2 = option.nextElementSibling
+    option2.innerText = "Megabyte"
+    let option3 = option2.nextElementSibling
+    option3.innerText = "Gigabyte"
+    let option4 = option3.nextElementSibling
+    option4.innerText = "Terabyte"
+  }
+
+  
 }
