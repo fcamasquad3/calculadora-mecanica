168ea1497afb8c913b3bb69dd36cc4675cf11995
Drielison Lopes <drielisonl@gmail.com>
2021-05-02 10:46:30 -0300

    JS loading

--- calcFreelancer/app.js
+++ calcFreelancer/app.js
@@ -1,21 +1,22 @@
-let valorTela = document.getElementById("tela")
+//IDEIA INICIAL
 
-//adiciona na tela da calculadora o caractere (número/operação) selecionado
-function adicionarNaTela (tela,caractere) {
-  if (tela.value == null || tela.value == 0) {
-    tela.value = caractere
-  } else {
-    tela.value += caractere
-  }
+let nomeJob = document.getElementById("nomeJob")
+
+let valorMes = document.getElementById("valorMes")
+let ValorHorasDia = document.getElementById("ValorHorasDia")
+let valorSemana = document.getElementById("valorSemana")
+let horasJob = document.getElementById("horasJob")
+let valorHoraTrabalho = document.getElementById("valorHoraTrabalho")
+
+//ex: 4000 = 6 / 4 / 4
+valorHoraTrabalho = valorMes.value / ValorHorasDia.value / valorSemana.value
+
+function calculate () {
+  let resultado = 0;
+
+  resultado = valorHoraTrabalho * horasJob;
+
+  return resultado
 }
 
-//pega a string formada na tela e computa o resultado
-function mostrarResultado() {
-  valorTela.value = eval(valorTela.value)
-}
-
-//deletar o último caractere inserido
-function deletar() {
-  let ultimo = valorTela.value.length - 1
-  valorTela.value = valorTela.value.slice(0, ultimo)
-}
+//valor do Job ${nome.Job}
