676beb4f72e8f463adee5397e24819f8a8c21527
barrosiam <samanta464.barros@gmail.com>
2021-05-01 21:54:01 -0300

    Primeira versão das funções de conversão de medida

--- calcConversor/app.js
+++ calcConversor/app.js
@@ -1,4 +1,6 @@
-const valorTela = document.getElementById("telaEntrada");
+let valorTela = document.getElementById("telaEntrada");
+let valorSaida = document.getElementById("telaSaida");
+let calculo = 0;
 
 // criação de arrays para function PopulacbMedidas()
 let pacoteMedidas = {
@@ -8,18 +10,18 @@ let pacoteMedidas = {
 }
 
 //chamada da função e desenvolvimento da mesma, populando os selects top-tipo
-$('#op-conversores').on('change', function() {
+$('#opConversores').on('change', function() {
 
   let selectValue = $(this).val();
 
-  $('#op-tipo1').empty();
-  $('#op-tipo2').empty();
+  $('#opTipo1').empty();
+  $('#opTipo2').empty();
   
   for (i = 0; i < pacoteMedidas[selectValue].length; i++) {
-    $('#op-tipo1').append("<option value='" + pacoteMedidas[selectValue][i] + "'>" + pacoteMedidas[selectValue][i] + "</option>");
+    $('#opTipo1').append("<option value='" + pacoteMedidas[selectValue][i] + "'>" + pacoteMedidas[selectValue][i] + "</option>");
   }
   for (i = 0; i < pacoteMedidas[selectValue].length; i++) {
-    $('#op-tipo2').append("<option value='" + pacoteMedidas[selectValue][i] + "'>" + pacoteMedidas[selectValue][i] + "</option>");
+    $('#opTipo2').append("<option value='" + pacoteMedidas[selectValue][i] + "'>" + pacoteMedidas[selectValue][i] + "</option>");
   }
 });
 
@@ -41,7 +43,69 @@ function mostrarResultado() {
 function deletar() {
   let ultimo = valorTela.value.length - 1
   valorTela.value = valorTela.value.slice(0, ultimo)
+  valorSaida.value = null;
 }
 
+function deletarConteudo() {
+  valorTela.value = 0;
+  valorSaida.value = 0;
+}
+
+function converte() {
+
+  let primeiroSelect = document.getElementById("opTipo1").value;
+  let segundoSelect = document.getElementById("opTipo2").value; 
+  
+  if (primeiroSelect == segundoSelect)
+  {
+     valorSaida.value = valorTela.value;
+  }  
+  
+  else if(primeiroSelect == "Quilômetro"){
+    switch(segundoSelect){
+      case "Milhas":
+        KmParaMilhas();
+      break;
+      case "Metro":
+        KmParaMetro();
+      break;
+      case "Pés":
+        KmParaPes();
+      break;
+      case "Centímetros":
+        KmParaCm();
+      break;
+      case "Polegadas":
+        KmParaPolegadas();
+      break;          
+    }
+  } 
+  
+}
+
+function KmParaMilhas(){   
+  calculo = valorTela.value / 1.609;
+  valorSaida.value = calculo.toFixed(4);  
+}
+
+function KmParaMetro(){
+  calculo = valorTela.value * 1000;
+  valorSaida.value = calculo;
+}
+
+function KmParaPes(){
+  calculo = valorTela.value * 3281;
+  valorSaida.value = calculo;
+}
+
+function KmParaCm(){
+  calculo = valorTela.value * 100000;
+  valorSaida.value = calculo;
+}
+
+function KmParaPolegadas(){
+  calculo = valorTela.value * 39370;
+  valorSaida.value = calculo;
+}
 
 
