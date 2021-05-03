4e36006895c7851434584070fb4499a454d79458
Drielison Lopes <drielisonl@gmail.com>
2021-05-03 12:39:52 -0300

    ajustes em tela
    organização nos códigos HTML e JS
    
    Alterações discutidas na daily 03/05
    aumentar o botao
    aumentar o nome calcular
    tirar a borda
    colocar padding-left no input

--- calcFreelancer/app.js
+++ calcFreelancer/app.js
@@ -1,8 +1,12 @@
-var  valorHorasDia = document.getElementById("valorHorasDia")
-var valorSemana = document.getElementById("valorSemana")
+var nomeJob = document.getElementById("nomeJob")
+var result = document.querySelector('#result');
+
 var valorMes = document.getElementById("valorMes")
-var valorHoraTrabalho = document.getElementById("valorHoraTrabalho")
+var valorHorasDia = document.getElementById("valorHorasDia")
+var valorSemana = document.getElementById("valorSemana")
 var horasJob = document.getElementById("horasJob")              
+
+var valorHoraTrabalho = document.getElementById("valorHoraTrabalho")
 var valorJob = document.getElementById("ValorJob")
 
 // Calcula valor das horas trabalhadas
@@ -15,14 +19,19 @@ function calcularValorHorasTrabalhadas(valorMes,valorHorasDia,valorSemana ){
 function calcularValorDoProjeto(valorHora, horasJob){
   calcularValorHorasTrabalhadas(valorMes,valorHorasDia,valorSemana)
   let resultado = valorHora    * horasJob.value
-  return resultado.toFixed(2)
+  return resultado.toFixed(2).replace(".", ",")
 }
 
 // Mostra resultado nos inputs valorHoraTrabalho e ValorJob ao 
 // pressiona o botao CALCULAR 
 function mostrarResultado() {
-  valorHoraTrabalho.value = calcularValorHorasTrabalhadas(valorMes,valorHorasDia,valorSemana)
+  if (nomeJob == '' || valorMes == '' || valorHorasDia == '' || valorSemana == '' || horasJob == '') {
+    alert("Por favor preencha todos os campos.")
+  } else {
+    valorHoraTrabalho.value = calcularValorHorasTrabalhadas(valorMes,valorHorasDia,valorSemana)
 
-  valorJob.value = calcularValorDoProjeto(
-  calcularValorHorasTrabalhadas(valorMes,valorHorasDia,valorSemana),horasJob)
+    valorJob.value = calcularValorDoProjeto(
+    calcularValorHorasTrabalhadas(valorMes,valorHorasDia,valorSemana),horasJob)
+    result.innerText = `Valor do projeto ${nomeJob.value.toUpperCase()} é de:`;
+  }
 }
