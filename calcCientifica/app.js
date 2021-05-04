768e75ddb843a8912a4672c671d9677b0b3995ae
Gisele Paiva <giselepaiva.arq@gmail.com>
2021-05-04 16:07:00 -0300

    loading div na científica

--- calcCientifica/app.js
+++ calcCientifica/app.js
@@ -161,3 +161,9 @@ function abre() {
 function fechar() {
   document.getElementById("menu-mobile").style.right = `${'-100vw'}`
 }
+
+// Carregamento da load-page
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var estilo = document.getElementsByClassName('load-page');
+  estilo[0].style.visibility = "hidden";
+});
